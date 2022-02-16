81537f734eac36ae65b5006e816c50896196062c
orhanakkurt <oakkurtny@gmail.com>
2022-02-15 22:26:51 -0500

    Initial commit

---
+++ src/test/java/io/swagger/client/api/intgetAverage.java
@@ -0,0 +1,44 @@
+//package io.swagger.client.api;
+//
+//public class intgetAverage {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public intgetAverage(int num) {
+//
+//        int res = 0;
+//        for (int i = 0; i < num; i++) {
+//
+//            res += i;
+//        }
+//    }
+//
+//
+////
+//        public static String FrequenceofChars(String str)  {
+//
+//            String nonDup= "hello";
+//
+//            for (int i=0; i<lentgnt
+//
+//        }
+//
+//
+//        String str= "hello";
+//
+//        public String[] answer(String str)
+//
+//
+//
+//
+//    }
+//
+//
+//
+//// h1e1l201  hello
+
+//      Questions asked:
+//        Get me the average of a number from its digits (ex: 123 = 1+2+3=6; 6 / 3)
+//        Get me the letters and their usage numbers within a String (ex: hello; return [h1, e1,l2,o1])
